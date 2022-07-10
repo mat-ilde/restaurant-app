92990cbe3a044a827779f349e8deb54f096d6512
mat-ilde <pascualsoto.matilde@gmail.com>
2022-07-10 21:29:27 +0100

    Creating project structure

---
+++ hospitality.manage/src/main/java/com/hospitality/app/hospitality/manage/Application.java
@@ -0,0 +1,13 @@
+package com.hospitality.app.hospitality.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+
+@SpringBootConfiguration
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(com.hospitality.app.hospitality.man.Application.class, args);
+	}
+
+}
